1e9f74eb20367b22d3c829cf69bea20b24dacf06
DilayNA <221903536@ogr.uludag.edu.tr>
2020-10-30 17:10:25 +0300

    odev1

---
+++ haft3odev1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace haft3odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+
+            Console.WriteLine("1.sayıyı giriniz.");
+            sayi1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("2.sayıyı giriniz.");
+            sayi2 = Convert.ToInt16(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+
+                Console.WriteLine("1.Sayı Büyük ");
+            else if (sayi2 > sayi1)
+                Console.WriteLine("2.Sayı Büyük ");
+            else
+                Console.WriteLine("İki sayı birbirine eşittir");
+
+            Console.ReadLine();
+        }
+    }
+}
